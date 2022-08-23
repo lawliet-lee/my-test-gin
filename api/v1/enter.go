6aba7516db4565ecc48f5d9666bc3f7931cd9a81
tli <tli@justhealth.cn>
2022-08-23 15:19:01 +0800

    feat:init

---
+++ api/v1/enter.go
@@ -0,0 +1,15 @@
+package v1
+
+import (
+	"github.com/flipped-aurora/gin-vue-admin/server/api/v1/example"
+	"github.com/flipped-aurora/gin-vue-admin/server/api/v1/my_test"
+	"github.com/flipped-aurora/gin-vue-admin/server/api/v1/system"
+)
+
+type ApiGroup struct {
+	SystemApiGroup  system.ApiGroup
+	ExampleApiGroup example.ApiGroup
+	My_testApiGroup my_test.ApiGroup
+}
+
+var ApiGroupApp = new(ApiGroup)
---
+++ service/enter.go
@@ -0,0 +1,15 @@
+package service
+
+import (
+	"github.com/flipped-aurora/gin-vue-admin/server/service/example"
+	"github.com/flipped-aurora/gin-vue-admin/server/service/my_test"
+	"github.com/flipped-aurora/gin-vue-admin/server/service/system"
+)
+
+type ServiceGroup struct {
+	SystemServiceGroup  system.ServiceGroup
+	ExampleServiceGroup example.ServiceGroup
+	My_testServiceGroup my_test.ServiceGroup
+}
+
+var ServiceGroupApp = new(ServiceGroup)
